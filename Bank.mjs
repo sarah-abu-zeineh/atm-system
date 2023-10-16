9d42c65bb9924de7eddefa23a382783a276cd7cf
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-16 14:04:09 +0300

    AS-7 Add the accounts and atms directly fron the Bank constructor

--- Bank.mjs
+++ Bank.mjs
@@ -1,19 +1,32 @@
 import {generateUniqueId} from "./helpers/helper.mjs";
+
 import {Account} from "./Account.mjs";
+import {ATM} from "./ATM.mjs"
+
+import {AccountsArray} from "./utils/AccountsArray.mjs";
+import { ATMs } from "./utils/ATMsArrya.mjs";
 
 export class Bank {
     constructor(bankName) {
         this.id = + generateUniqueId();
-        this.accounts = [];
-        this.atms = [];
+        this.accounts = AccountsArray.map(account => {
+            return this.createAccount(account)
+        });
+        this.atms = ATMs.map(atm => {
+            return this.addATM(atm);
+        });
         this.bankName = bankName;
     }
 
     createAccount(account) {
-        this.accounts.push(new Account(account));
+        return new Account(account)
     }
 
-    getAccounts() {}
+    getAccount() {}
+
+    addATM(atm){
+        return new ATM(atm.balance, atm.location)
+    }
 
     deleteAccount() {}
 }
