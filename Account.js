0c0a268f42c10f51c4b74228119614c1cb12eef8
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-16 11:18:22 +0300

    AS-4 Implement the structure of the Accounts class

---
+++ Account.js
@@ -0,0 +1,21 @@
+class Account {
+    constructor(userName, firstName, lastName, password, balance, currencyType) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+        this.lastInteract = {};
+        this.currencyType = currencyType;
+    }
+
+    cashWithDraw() {}
+
+    cashDeposit() {}
+
+    changePassword() {}
+
+    transferFund() {}
+
+}
