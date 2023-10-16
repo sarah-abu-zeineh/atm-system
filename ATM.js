e73af76e7c92dd81e6b6cdbdd22077cd2c79687e
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-16 11:10:26 +0300

    AS-4 Create the stucture of ATM class

---
+++ ATM.js
@@ -0,0 +1,17 @@
+class ATM {
+    constructor(location) {
+        this.id = id;
+        this.balance = 0;
+        this.location = location;
+    }
+
+    displayMenu() {}
+
+    logIn() {}
+
+    logOut() {}
+
+    convertCurrency() {}
+
+    exit() {}
+}
