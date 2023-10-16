bd8dd0ae884909ea0591832999ce78ad45e81ae5
meera-iyad <191128@ppu.edu.ps>
2023-10-16 13:19:39 +0300

    AS-3 implement-login-method

--- UserInterface.js
+++ UserInterface.js
@@ -1,6 +1,31 @@
+import { accounts } from "./utils/AccountsArray.js";
+import * as readline from "readline";
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 export class UserInterface {
     constructor() {
     }
+    login() {
+        rl.question('Enter your username: ', (username) => {
+            rl.question('Enter your password: ', (password) => {
+                const isValidCredentials = accounts.some(account => {
+                    return account.user_name === username && account.password === password;
+                });
 
-    display() {}
+                if (isValidCredentials) {
+                    console.log('Login successful.');
+                } else {
+                    console.log('Login failed.');
+                }
+                rl.close();
+            });
+        });
+    }
+    display() { }
 }
+
