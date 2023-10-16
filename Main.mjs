0b0ebfb66d46451ef4d3ee46cfde00e508117628
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-16 14:07:49 +0300

    AS-7 Clean the Main file

--- Main.mjs
+++ Main.mjs
@@ -1,20 +1,3 @@
 import {Bank} from "./Bank.mjs";
-import {AccountsArray} from './utils/AccountsArray.mjs';
 
 const myBank = new Bank("Hebron Islamic Bank");
-
-const account = {
-    "userName": "cyurukhin0",
-    "password": "eG0<()_E",
-    "firstName": "Cherlyn",
-    "lastName": "Yurukhin",
-    "email": "cyurukhin0@walmart.com",
-    "gender": "Female",
-    "lastInteract": "3/20/2023",
-    "currencyType": "$"
-}
-
-AccountsArray.forEach(account => {
-    myBank.createAccount(account)
-})
-console.log(myBank.accounts)
