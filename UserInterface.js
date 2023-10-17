3e4a2cfd2dec107f916f5091d63ffef1d84006e8
meera-iyad <191128@ppu.edu.ps>
2023-10-17 12:03:40 +0300

    AS-1-Complete-cash-withdraw-feature

--- UserInterface.js
+++ UserInterface.js
@@ -46,7 +46,7 @@ export class UserInterface {
             switch (choice) {
                 case '1': this.myBank.atms[0].currentAccount.displayBalance();
                     break;
-                case '2': 
+                case '2': this.cashWithDrawMenue();
                     break;
                 case '3':
                     break;
@@ -64,7 +64,48 @@ export class UserInterface {
             this.handleMenuSelection();
         });
     }
+    cashWithDrawMenue() {
+        console.log(' Select Amount ');
+        console.log(`1. ${this.myBank.atms[0].currentAccount.currencyType.icon}100`);
+        console.log(`2. ${this.myBank.atms[0].currentAccount.currencyType.icon}200`);
+        console.log(`3. ${this.myBank.atms[0].currentAccount.currencyType.icon}500`);
+        console.log(`4. ${this.myBank.atms[0].currentAccount.currencyType.icon}700`);
+        console.log('5. other');
+        console.log('6. back');
+        this.handleWithdrawMenuSelection();
+    }
+    handleWithdrawMenuSelection() {
+        rl.question('Enter your choice: ', async (choice) => {
+            switch (choice) {
+                case '1': this.myBank.atms[0].currentAccount.cashWithDraw(100);
+                    break;
+                case '2': this.myBank.atms[0].currentAccount.cashWithDraw(200);
+                    break;
+                case '3': this.myBank.atms[0].currentAccount.cashWithDraw(500);
+                    break;
+                case '4': this.myBank.atms[0].currentAccount.cashWithDraw(700);
+                    break;
+                case '5':
+                    const amount = await this.askUserForAmount();
+                    this.myBank.atms[0].currentAccount.cashWithDraw(amount);
+                    break;
+                case '6':
+                    break;
+                default:
+                    console.log('Invalid choice. Please try again.');
+            }
+            this.displayMenu();
+        });
 
+    }
+
+    async askUserForAmount() {
+        return new Promise((resolve) => {
+            rl.question('Enter the amount you want to deposit: ', (amount) => {
+                resolve(Number(amount));
+            });
+        });
+    }
 
 
 
