9142c77c2a666b9baea3f1cda3fd5f04f9eeb4d2
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-28 22:11:56 +0200

    #30 AS-17 Remove redundant comments

--- Main.js
+++ Main.js
@@ -3,11 +3,6 @@ import {Bank} from "./Bank.js";
 
 import { UserInterface } from "./UserInterface.js";
 
-const userInerface = new UserInterface();
-userInerface.run();
-
-// const myBank = new Bank("Hebron Islamic Bank");
-
-// console.log(myBank.isAccountValid("cyurukhin0"))
-
+const userInterface = new UserInterface();
+userInterface.run();
 
