35ef54df080c7c3a747da43f4ff30f58abbc5d81
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-30 00:28:32 +0200

    #30 AS-18 Organize constants in a dedicated file

--- UserInterface.js
+++ UserInterface.js
@@ -4,6 +4,7 @@ import {Authenticator} from "./Authenticator.js";
 import {PasswordValidator} from "./PasswordValidator.js";
 import {TransactionManager} from "./TransactionManager.js";
 import {CurrencyConverter} from "./CurrencyConverter.js";
+import {MenuOptions, currencyOptions, WithdrawalOptions} from './constants.js'
 
 import * as readline from "readline";
 
@@ -294,27 +295,3 @@ export class UserInterface {
         this.login();
     }
 }
-
-const MenuOptions = {
-    BALANCE_INQUIRY: '1',
-    CASH_WITHDRAWAL: '2',
-    CASH_DEPOSIT: '3',
-    TRANSFER_FUND: '4',
-    CHANGE_PASSWORD: '5',
-    EXIT: '6'
-};
-
-const currencyOptions = {
-    DOLLAR: '1',
-    DINAR: '2',
-    ILS: '3'
-};
-
-const WithdrawalOptions = {
-    OPTION_1: '1',
-    OPTION_2: '2',
-    OPTION_3: '3',
-    OPTION_4: '4',
-    CUSTOM: '5',
-    BACK: '6'
-};
